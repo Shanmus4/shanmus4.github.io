10dfcb342b60a3b140a1bd697c606324ab0838f0
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-03-11 02:10:57 +0530

    added tooltip images

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,10 +1,10 @@
 const gradientsabout = [
   "linear-gradient(30deg, #6db0db 30%, #3a7d92 60%)",
-  "linear-gradient(50deg, #72c1d7 30%, #3a7d92 60%)",
+  "linear-gradient(50deg, #72c1d7 0%, #3a7d92 60%)",
   "linear-gradient(45deg, #60a4d4 30%, #1d6b7e 60%)",
   "linear-gradient(50deg, #6592d9 30%, #1e667c 60%)",
   "linear-gradient(60deg, #76b0d7 30%, #1f7b93 60%)",
-  "linear-gradient(45deg, #5ab2cc 30%, #2b6d80 60%)",
+  "linear-gradient(45deg, #5ab2cc 30%, #2b6d80 60%)"
 ];
 const randomIndex = Math.floor(Math.random() * 6);
 const about = document.getElementsByClassName("about")[0];
@@ -19,5 +19,3 @@ LottieInteractivity.create({
     },
   ],
 });
-
-
