155885260f3e917c472a27a9f9ddc6b16b653736
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-03-10 16:10:48 +0530

    Added Gallery final

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,51 +1,23 @@
 const gradientsabout = [
-  "linear-gradient(45deg, #00cdac 10%, #02aab0 60%)",
-  "linear-gradient(45deg, #eaafc8 15%, #654f43 80%)",
-  "linear-gradient(45deg, #6ec792 10%, #35805c 90%)",
-  "linear-gradient(45deg, #ffb260 10%, #c06e30 90%)",
-  "linear-gradient(45deg, #ff9fed 10%, #d161ca 50%)",
+  "linear-gradient(30deg, #6db0db 30%, #3a7d92 60%)",
+  "linear-gradient(50deg, #72c1d7 30%, #3a7d92 60%)",
+  "linear-gradient(45deg, #60a4d4 30%, #1d6b7e 60%)",
+  "linear-gradient(50deg, #6592d9 30%, #1e667c 60%)",
+  "linear-gradient(60deg, #76b0d7 30%, #1f7b93 60%)",
+  "linear-gradient(45deg, #5ab2cc 30%, #2b6d80 60%)",
 ];
-
-const randomIndex = Math.floor(Math.random() * 5);
+const randomIndex = Math.floor(Math.random() * 6);
 const about = document.getElementsByClassName("about")[0];
 about.style.background = gradientsabout[randomIndex];
 
-const scroller = document.querySelector('.gallery_scroller');
-const dotsContainer = document.querySelector('.dot-indicators');
-
-// create dots
-for (let i = 0; i < scroller.children.length-1; i++) {
-  const dot = document.createElement('div');
-  dot.classList.add('dot');
-  if (i === 0) {
-    dot.classList.add('active');
-  }
-  dot.addEventListener('click', () => {
-    const scrollAmount = i * scroller.children[0].offsetWidth;
-    scroller.scrollTo({
-      left: scrollAmount,
-      behavior: 'smooth'
-    });
-    // update active dot
-    const activeDot = dotsContainer.querySelector('.active');
-    activeDot.classList.remove('active');
-    dot.classList.add('active');
-  });
-  dotsContainer.appendChild(dot);
-}
-
-// update active dot on scroll
-scroller.addEventListener('scroll', () => {
-  const scrollAmount = scroller.scrollLeft;
-  const activeIndex = Math.round(scrollAmount / scroller.children[0].offsetWidth);
-  const activeDot = dotsContainer.querySelector('.active');
-  activeDot.classList.remove('active');
-  const newActiveDot = dotsContainer.children[activeIndex];
-  newActiveDot.classList.add('active');
+LottieInteractivity.create({
+  player: "#footballlottie",
+  mode: "cursor",
+  actions: [
+    {
+      type: "toggle",
+    },
+  ],
 });
-
-
-
-
 
 
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,19 +1,23 @@
 const gradientshome = [
-  "linear-gradient(45deg, #6e8a78 10%, #3d4f62 50%)",
-  "linear-gradient(45deg, #c8b8aa 10%, #4a3d3a 60%)",
-  "linear-gradient(45deg, #5385c2 50%, #8aa9ce 70%)",
-  "linear-gradient(45deg, #845b4f 10%, #866c63 70%)",
-  "linear-gradient(45deg, #777185 2%, #c9cc93 90%)",
+  "linear-gradient(45deg, #6881a8 10%, #364d6c 50%)",
+  "linear-gradient(60deg, #6c7b9f 10%, #3e4e6d 50%)",
+  "linear-gradient(75deg, #6d769a 10%, #45517a 50%)",
+  "linear-gradient(90deg, #6e738e 10%, #4a4e6b 50%)",
+  "linear-gradient(45deg, #596c86 80%, #2d4a5b 90%)",
 ];
-
 const home = document.getElementsByClassName("home")[0];
-
-
-
 
 // Generate a random number to select a gradient from the array
 const randomIndex = Math.floor(Math.random() * 5);
 
-
 home.style.background = gradientshome[randomIndex];
 
+LottieInteractivity.create({
+  player: "#footballlottie",
+  mode: "cursor",
+  actions: [
+    {
+      type: "toggle",
+    },
+  ],
+});
