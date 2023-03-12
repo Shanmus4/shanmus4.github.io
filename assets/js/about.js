de11f16b8c4b87b8936481aff3500b2889182f18
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-03-13 00:47:40 +0530

    Done finally

--- assets/js/about.js
+++ assets/js/about.js
@@ -4,7 +4,7 @@ const gradientsabout = [
   "linear-gradient(45deg, #60a4d4 30%, #1d6b7e 60%)",
   "linear-gradient(50deg, #6592d9 30%, #1e667c 60%)",
   "linear-gradient(60deg, #76b0d7 30%, #1f7b93 60%)",
-  "linear-gradient(45deg, #5ab2cc 30%, #2b6d80 60%)"
+  "linear-gradient(45deg, #5ab2cc 30%, #2b6d80 60%)",
 ];
 const randomIndex = Math.floor(Math.random() * 6);
 const about = document.getElementsByClassName("about")[0];
