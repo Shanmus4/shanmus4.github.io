cfa4b01547e787c9c005355d0840398b961ea931
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-06-04 14:13:58 +0530

    Colors changed

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,14 +1,14 @@
-const gradientsabout = [
-  "linear-gradient(30deg, #6db0db 30%, #3a7d92 60%)",
-  "linear-gradient(50deg, #72c1d7 0%, #3a7d92 60%)",
-  "linear-gradient(45deg, #60a4d4 30%, #1d6b7e 60%)",
-  "linear-gradient(50deg, #6592d9 30%, #1e667c 60%)",
-  "linear-gradient(60deg, #76b0d7 30%, #1f7b93 60%)",
-  "linear-gradient(45deg, #5ab2cc 30%, #2b6d80 60%)",
-];
-const randomIndex = Math.floor(Math.random() * 6);
-const about = document.getElementsByClassName("about")[0];
-about.style.background = gradientsabout[randomIndex];
+// const gradientsabout = [
+//   "linear-gradient(30deg, #6db0db 30%, #3a7d92 60%)",
+//   "linear-gradient(50deg, #72c1d7 0%, #3a7d92 60%)",
+//   "linear-gradient(45deg, #60a4d4 30%, #1d6b7e 60%)",
+//   "linear-gradient(50deg, #6592d9 30%, #1e667c 60%)",
+//   "linear-gradient(60deg, #76b0d7 30%, #1f7b93 60%)",
+//   "linear-gradient(45deg, #5ab2cc 30%, #2b6d80 60%)",
+// ];
+// const randomIndex = Math.floor(Math.random() * 6);
+// const about = document.getElementsByClassName("about")[0];
+// about.style.background = gradientsabout[randomIndex];
 
 LottieInteractivity.create({
   player: "#footballlottie",
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,16 @@
-const gradientshome = [
-  "linear-gradient(45deg, #6881a8 10%, #364d6c 50%)",
-  "linear-gradient(60deg, #6c7b9f 10%, #3e4e6d 50%)",
-  "linear-gradient(75deg, #6d769a 10%, #45517a 50%)",
-  "linear-gradient(90deg, #6e738e 10%, #4a4e6b 50%)",
-  "linear-gradient(45deg, #596c86 80%, #2d4a5b 90%)",
-];
-const home = document.getElementsByClassName("home")[0];
+// const gradientshome = [
+//   "linear-gradient(45deg, #6881a8 10%, #364d6c 50%)",
+//   "linear-gradient(60deg, #6c7b9f 10%, #3e4e6d 50%)",
+//   "linear-gradient(75deg, #6d769a 10%, #45517a 50%)",
+//   "linear-gradient(90deg, #6e738e 10%, #4a4e6b 50%)",
+//   "linear-gradient(45deg, #596c86 80%, #2d4a5b 90%)",
+// ];
+// const home = document.getElementsByClassName("home")[0];
 
-// Generate a random number to select a gradient from the array
-const randomIndex = Math.floor(Math.random() * 5);
+// // Generate a random number to select a gradient from the array
+// const randomIndex = Math.floor(Math.random() * 5);
 
-home.style.background = gradientshome[randomIndex];
+// home.style.background = gradientshome[randomIndex];
 
 LottieInteractivity.create({
   player: "#footballlottie",
